f80e9ba236b4af7285a44b681a9423f3c0d1d24a
foamvalue <foamvalue@live.cn>
2017-03-13 19:46:53 +0800

    index

--- vertx-press/vertx-manage/src/main/java/io/vertPress/manage/ManageServer.java
+++ vertx-press/vertx-manage/src/main/java/io/vertPress/manage/ManageServer.java
@@ -9,6 +9,11 @@ import io.vertx.ext.asyncsql.AsyncSQLClient;
 import io.vertx.ext.asyncsql.MySQLClient;
 import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.CookieHandler;
+import io.vertx.ext.web.handler.SessionHandler;
+import io.vertx.ext.web.sstore.LocalSessionStore;
+import io.vertx.ext.web.templ.FreeMarkerTemplateEngine;
 
 /**
  * @ClassName: ManageServer
@@ -23,6 +28,11 @@ public class ManageServer extends AbstractVerticle {
 	 * @Fields LOGGER : 日志对象
 	 */
 	private final static Logger LOGGER = LoggerFactory.getLogger(ManageServer.class);
+	
+	/**
+	 * @Fields ENGINE : 模版引擎
+	 */
+	private final static FreeMarkerTemplateEngine ENGINE = FreeMarkerTemplateEngine.create();
 
 	/**
 	 * @Fields MYSQL : 数据库对象
@@ -61,7 +71,13 @@ public class ManageServer extends AbstractVerticle {
 	public void start() throws Exception {
 		// 主路由
 		Router router = Router.router(vertx);
+		
+		// 缓存
+		router.route().handler(CookieHandler.create());
+		router.route().handler(BodyHandler.create());
+		router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx)));
 
+		
 		// 初始化数据库对象
 		sqlClient = MySQLClient.createShared(vertx, MYSQL);
 
@@ -90,10 +106,21 @@ public class ManageServer extends AbstractVerticle {
 		// 子路由 - 管理服务
 		Router manageRouter = Router.router(vertx);
 
+		manageRouter.route().handler(routingContext -> {
+			routingContext.put("name", "Vert.x Web");
+			ENGINE.render(routingContext, "templates/index.ftl", res -> {
+				if (res.succeeded()) {
+					routingContext.response().end(res.result());
+				} else {
+					routingContext.fail(res.cause());
+				}
+			});
+		});
+
 		// 设置子路由到主路由
 		router.mountSubRouter("/init", initRouter);
 		router.mountSubRouter("/manage", manageRouter);
-
+		
 		LOGGER.debug("ManageServer is running.");
 		// 启动服务
 		vertx.createHttpServer().requestHandler(router::accept).listen(SERVER_PORT);
---
+++ vertx-press/vertx-manage/src/main/java/io/vertPress/manage/auth/AuthHandle.java
@@ -0,0 +1,11 @@
+package io.vertPress.manage.auth;
+
+import io.vertx.ext.web.RoutingContext;
+
+public class AuthHandle {
+
+	public void handleGetProduct(RoutingContext routingContext) {
+		
+		
+	}
+}
