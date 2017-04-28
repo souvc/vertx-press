fdfba1a7258d807492d7799e64b3e39fb6263e33
foamvalue <foamvalue@live.cn>
2017-04-28 16:57:19 +0800

    add properties

---
+++ vertx-press/vertx-weixin/src/main/java/io/vertPress/weixin/WeixinServer.java
@@ -0,0 +1,31 @@
+package io.vertPress.weixin;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+/**
+ * @ClassName: WeixinServer
+ * @Description: TODO 微信模块
+ * @author FoamValue foamvalue@live.cn
+ * @date 2017年4月28日 下午4:45:00
+ * 
+ */
+public class WeixinServer extends AbstractVerticle {
+
+	/**
+	 * @Fields LOGGER : 日志对象
+	 */
+	private final static Logger LOGGER = LoggerFactory.getLogger(WeixinServer.class);
+
+	@Override
+	public void start() throws Exception {
+		
+		
+		
+		
+		
+		
+	}
+
+}
